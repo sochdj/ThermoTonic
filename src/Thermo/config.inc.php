75437b68f1d1328ada86da09c269dc417ae0989b
sochdj88 <sochdj88@gmail.com>
2016-03-29 21:59:29 +0200

    Corretta funzione di lettura fasce orarie

--- src/Thermo/config.inc.php
+++ src/Thermo/config.inc.php
@@ -3,12 +3,12 @@
 define('DB_SERVER', "localhost");
 
 //database login name
-define('DB_USER', "Thermo");
+define('DB_USER', "thermo");
 //database login password
-define('DB_PASS', "Thermo");
+define('DB_PASS', "thermo");
 
 //database name
-define('DB_DATABASE', "Thermo");
+define('DB_DATABASE', "thermo");
 
 //smart to define your table names also
 define('TABLE_TEMPERATURE', "Temperature");
--- web/script/app/setupController.js
+++ web/script/app/setupController.js
@@ -1,20 +1,43 @@
 appRoot.controller('setupController', ['$scope', 'apiService',
     function ($scope, apiService) {
-        $scope.ranges = {
+        /* $scope.ranges = {
             dayOfWeek: 0,
             fascia: 0,
             timeStart: "",
             timeEnd: "",
             setPoint: 0,
             hystersis: 0,
-        };
+         };*/
 
         $scope.giorno = "1";
 
+        $scope.ranges = [];
+
         $scope.$watch('giorno', function (newValue) {
             apiService.getTimeRanges(newValue).then(function (data) {
-                $scope.ranges = data.ranges;
+                //$scope.ranges = data.ranges;
+                $scope.ranges.length = 0;
+                angular.forEach(data.ranges, function (range) {
+                    var initSplit = range.timeStart.split(/[:]/);
+                    var endSplit = range.timeEnd.split(/[:]/);
+                    range.initHour = parseInt(initSplit[0]);
+                    range.initMinute = parseInt(initSplit[1]);
+                    range.endHour = parseInt(endSplit[0]);
+                    range.endMinute = parseInt(endSplit[1]);
+
+                    $scope.ranges.push(range);
+                })
+
             }, function (reason) {
             });
         });
+
+
+        $scope.updateInit = function (init) {
+            init.timeStart = twoDigits(init.initHour) + ":" + twoDigits(init.initMinute) + ":00";
+        }
+        $scope.updateEnd = function (end) {
+            end.timeEnd = twoDigits(end.endHour) + ":" + twoDigits(end.endMinute) + ":00";
+        }
+
     }]);
